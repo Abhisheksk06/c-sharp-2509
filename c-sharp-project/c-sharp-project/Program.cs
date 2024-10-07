97ec4d8c4251cf4e19ea6a7ac116d5026d3ac667
Abhisheksk06 <abhisheksk0606@gmail.com>
2024-10-07 09:54:01 +0530

    added class notes

--- c-sharp-project/c-sharp-project/Program.cs
+++ c-sharp-project/c-sharp-project/Program.cs
@@ -10,6 +10,113 @@ namespace c_sharp_project
     {
         static void Main(string[] args)
         {
+            //for(int i=1;i<=11;i+=2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadLine();
+
+            //Console.WriteLine("Enter the no. of rows");
+            //int rows = Convert.ToInt32(Console.ReadLine());
+            //for(int i = 1; i <= rows; i++)
+            //{
+            //    for(int j=1; j<=i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+            //Console.ReadLine();
+
+
+
+
+
+            //int sum = 0;
+            //for(int i = 1; i <= 5; i++)
+            //{
+            //    sum += i;
+            //}
+            //Console.WriteLine(sum);
+            //Console.ReadLine();
+
+
+
+
+
+
+
+            //int rows = 4;
+            //int number = 1;
+            //int space = rows + 4;
+
+            //for (int i = 1; i <= rows; i++)
+            //{
+            //    for (int k = space; k >= 1; k--)
+            //    {
+            //        Console.Write("  ");
+            //    }
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        if (number <= 10)
+            //        {
+            //            Console.Write(number + " ");
+            //            number++;
+            //        }
+            //    }
+            //    Console.WriteLine("\n");
+            //    space = space - 1;
+            //}
+            //Console.ReadLine();
+
+
+
+
+
+
+            //for(int i = 10; i <= 190; i += 20)
+            //{
+            //    if(i % 20 == 0)
+            //    {
+            //        continue;
+            //    }
+            //    Console.WriteLine(i);
+            //    if(i > 190)
+            //    {
+            //        break;
+            //    }
+            //}
+            //Console.ReadLine();
+
+
+
+            //Console.WriteLine("Enter a number");
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //switch (number % 2)
+            //{
+            //    case 0:
+            //        Console.WriteLine(number + " is an even number");
+            //        break;
+            //    case 1:
+            //        Console.WriteLine(number + " is an odd number");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Enter a valid number");
+            //        break;
+            //}
+            //Console.ReadLine();
+            //Console.Readline();
+
+
+
+            //int[] arr = { 10, 20, 30, 40, 50 };
+            //for(int i=0;i<arr.Length;i++)
+            //{
+            //    Console.WriteLine(arr[i]);
+            //}
+            //Console.ReadLine();
         }
     }
 }
