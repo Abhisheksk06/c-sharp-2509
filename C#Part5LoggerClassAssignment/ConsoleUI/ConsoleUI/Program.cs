5f996715c1f41efa5b15f619b3c7c26251d7c5fe
Abhisheksk06 <115362492+Abhisheksk06@users.noreply.github.com>
2024-11-03 20:02:36 +0530

    Added Logger class Assignment of C# part 5

---
+++ C#Part5LoggerClassAssignment/ConsoleUI/ConsoleUI/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//[assembly: log4net.Config.XmlConfigurator(Watch =true)]
+
+namespace ConsoleUI
+{
+    public class Program
+    {
+        //private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public static void Divide(int a, int b)
+        {
+            try
+            {
+                int result = a / b;
+                Console.WriteLine("Result: " + result);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Logger.LogError("Attempted to divide by zero.", ex);
+                Console.WriteLine("DivideByZeroException caught and logged.");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Divide(10, 0);
+            Console.ReadLine();
+        }
+    }
+}
